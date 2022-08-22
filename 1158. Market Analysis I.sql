38239166fcaa038673811b6731dceba249794c25
Sana2786 <61844429+Sana2786@users.noreply.github.com>
2022-08-22 23:26:13 +0530

    Create 1158. Market Analysis I.sql

---
+++ 1158. Market Analysis I.sql
@@ -0,0 +1,8 @@
+#Runtime: 1124 ms, faster than 71.68% of MySQL online submissions for Market Analysis I.
+select user_id as buyer_id, join_date, 
+    sum(case when order_id is not null then 1 else 0 end) as orders_in_2019
+from users
+left join orders on users.user_id = orders.buyer_id 
+and year(order_date) = 2019
+group by user_id, join_date
+order by buyer_id
