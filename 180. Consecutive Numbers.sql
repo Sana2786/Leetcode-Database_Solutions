6af94dea71826d761cd2478a6922158e5d7ffc7c
Sana2786 <61844429+Sana2786@users.noreply.github.com>
2022-08-29 00:08:44 +0530

    Create 180. Consecutive Numbers.sql

---
+++ 180. Consecutive Numbers.sql
@@ -0,0 +1,4 @@
+#Runtime: 428 ms, faster than 90.37% of MySQL online submissions for Consecutive Numbers.
+select distinct l1.Num as ConsecutiveNums
+from Logs l1, Logs l2, Logs l3
+where l2.Id = l1.Id+1 and l3.Id = l2.Id + 1 and l1.Num = l2.Num and l2.Num = l3.Num
