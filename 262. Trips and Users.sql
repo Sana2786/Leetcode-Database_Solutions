a52caf3ac6c1702e022197c7733442256315b9eb
Sana2786 <61844429+Sana2786@users.noreply.github.com>
2022-08-30 23:49:59 +0530

    Create 262. Trips and Users.sql

---
+++ 262. Trips and Users.sql
@@ -0,0 +1,12 @@
+#Runtime: 584 ms, faster than 56.99% of MySQL online submissions for Trips and Users.
+select t.Request_at as Day, round(1 - sum(t.status = "Completed") / t.all_count, 2) as "Cancellation Rate"
+from (select *, count(status) over (partition by request_at) as all_count
+     from Trips
+     where client_id in (select users_id
+                        from Users
+                        where banned = "No") and
+            driver_id in (select users_id
+                        from Users
+                        where banned = "No")) as t
+where t.request_at in ('2013-10-01','2013-10-02','2013-10-03') 
+group by t.request_at
